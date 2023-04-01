0f7a68c0d69a615a5e85f3c7a6066753edea34c8
ana <analaura.chipweb@gmail.com>
2023-04-01 17:59:54 -0300

    refazendo estrutura de decisão

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-
+import random
 
 print("Seja bem vindo à plataforma de descarte da Urbitável. Deseja fazer o seu cadastro?")
 resposta = input()
@@ -30,8 +30,15 @@ else:
                 decisãoUsuario = input()
 
                 #estrutura menu programa
+                pontosAcumulados = random.randint(0,5.000)
+                comprovanteDeDescarte = ""
                 if decisãoUsuario == "realizar saque":
-                    print("Ok! Qual seria a sua forma de realizar o saque,",usuario,"?" " >>> OPÇÕES: PIX | TRANSAÇÃO BANCÁRIA <<<")
+                    print(f"{usuario}, você tem o total de {pontosAcumulados} pontos. Deseja prosseguir a retirada?")
+                    respostaRetirada = input()
+                    if respostaRetirada == "sim":
+                        print("Ok! Qual seria a sua forma de realizar o saque,>>> OPÇÕES: PIX | TRANSAÇÃO BANCÁRIA <<<")
+                    else:
+                        print(f"Ok! Programa encerrado. Até mais, {usuario}")
                     formadepag = input()
                     if formadepag == "pix":
                         print("Certo! Registre sua chave PIX e faremos o depósito em até 24h após a solicitação. Ajudo em algo mais?")
@@ -40,14 +47,16 @@ else:
                     respostajuda = input()
                     if respostajuda == "sim":
                         print("O que deseja fazer em nossa plataforma?  REALIZAR SAQUE | FALE CONOSCO | SOBRE NÓS | SAIR DA MINHA CONTA")
+                    elif decisãoUsuario == "fale conosco":
+                        print(f"Mande um email para o nosso suporte para que possamos ajudá-lo(a), {usuario}. O nosso email é urbitável@atendimento.com.br.")
+                    elif decisãoUsuario == "sobre nós":
+                        print("O projeto Urbitável é uma iniciativa coletiva de estudantes da FIAP, que busca apresentar uma solução sustentável, relativa a um dos problemas ambientais mais ocorridos pelo mundo todo: O inadequado descarte dos lixos e substratos prejudiciais ao ambiente em meiosurbanos. Se trata, portanto,de uma ideia interventiva que tem como objetivo recompensar a população urbana por descartar corretamente os seus lixos,através do programa consciente de descarte.")
+                    elif decisãoUsuario == "sair da minha conta":
+                        print(f"Você saiu da sua conta. Até mais, {usuario}!")
                     else:
-                        print("Ok! Programa encerrado.")
-                elif decisãoUsuario == "fale conosco":
-                    print(f"Mande um email para o nosso suporte para que possamos ajudá-lo(a), {usuario}. O nosso email é urbitável@atendimento.com.br.")
-                elif decisãoUsuario == "sobre nós":
-                    print("O projeto Urbitável é uma iniciativa coletiva de estudantes da FIAP, que busca apresentar uma solução sustentável, relativa a um dos problemas ambientais mais ocorridos pelo mundo todo: O inadequado descarte dos lixos e substratos prejudiciais ao ambiente em meiosurbanos. Se trata, portanto,de uma ideia interventiva que tem como o bjetivo recompensar a população urbana por descartar corretamente os seus lixos,através do programa consciente de descarte.")
-                elif decisãoUsuario == "sair da minha conta":
-                    print("Você saiu da sua conta. Até mais!")
+                        print(f"Ok! Programa encerrado. Até mais, {usuario}")
+
+                    
                      
 
 
