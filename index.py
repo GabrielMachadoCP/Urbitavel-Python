c5d881435a6d79a66e03224907a229af420748e3
bdancs <rm551645@fiap.com.br>
2023-04-04 18:18:03 -0300

    Alterações bobas porque já completaram

--- index.py
+++ index.py
@@ -1,29 +1,33 @@
 print("Seja bem vindo à plataforma de descarte da Urbitável. Deseja fazer o seu cadastro?")
 resposta = input()
 
-if resposta == "não":
-    print("programa encerrado")
+if resposta.lower() == "não":
+    print("Ok! Programa encerrado.")
 else:
-    print("digite seu novo usuario")
+    print("Digite seu novo usuário:")
     usuario = input()
-    print("ok! agora digite sua nova senha.")
+    print("Agora digite sua nova senha:")
     senha = int(input())
-    print("cadastro feito. Deseja fazer o login?")
+    print("Cadastro feito. Deseja fazer o login?")
     respostaLogin = input()
-    if respostaLogin == "não":
-        print("programa encerrado")
+    
+    if respostaLogin.lower() == "não":
+        print(f"Ok! Programa encerrado. Até mais {usuario}!")
     else:
-        print("insira seu usuario")
+        print("Insira seu usuário:")
         login = input()
-        if login != usuario:
-            print("Usuário incorreto. Tente novamente.")
+
+        if login.lower() != usuario:
+            print("Usuário incorreto, tente novamente.")
         else:
-            print("ok! agora digite sua senha.")
+            print("Ok! Agora digite sua senh:")
             senhaLogin = int(input())
+
             if senhaLogin != senha:
-                print("senha incorreta, digite novamente")
+                print("Senha incorreta, tente novamente.")
             else:
-                print("Login feito. Seja bem vindo,", usuario, "!")
+                print("Login feito. Seja bem-vindo, {usuario}!")
+                
 print("o que deseja fazer em nossa plataforma? REALIZAR SAQUE | FALE CONOSCO | SOBRE NÓS")
 decisãoUsuario = input()
 
